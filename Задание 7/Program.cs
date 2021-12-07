b63b451bdd4db9a6a86ec278b239c55c249fa92d
Aleksandr Panarin <panarinalex12@gmail.com>
2021-12-07 18:56:39 +0300

    Добавьте файлы проекта.

---
+++ Задание 7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Инженер! \n введите длины в милиметрах");
+            Console.WriteLine("Введите X");
+            double X = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Y");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Z");
+            double Z = Convert.ToDouble(Console.ReadLine());
+            AreaTriangle(X, Y, Z);
+            Console.ReadKey();
+        }
+
+        static void AreaTriangle(double X, double Y, double Z)
+        {
+            double p = (double)((X + Y + Z) / 2);
+            double S = Math.Sqrt(p * (p - X) * (p - Y) * (p - Z));
+            Console.WriteLine("Площадь треугольника S = {0} кв. мм", S);
+        }
+    }
+}
---
+++ Площадь куба и длина ребра/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Площадь_куба_и_длина_ребра
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Инженер! \n введите длину ребра куба в милиметрах");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double V, S;
+            cubeVolar(a, out S, out V);
+            Console.WriteLine("Площадь поверхности куба S = {0} кв. мм", S);
+            Console.WriteLine("Объем куба S = {0} кв. мм", V);
+            Console.ReadKey();
+        }
+
+        static void cubeVolar(double a, out double S, out double V)
+        {
+            S = Math.Pow(a, 2) * 6;
+            V = Math.Pow(a , 3);
+        }
+    }
+}
